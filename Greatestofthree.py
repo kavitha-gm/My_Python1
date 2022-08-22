9af3989b6d9b9b4025cbf71e101f3cc54481c321
kavitha-gm <kavithagmcs@gmail.com>
2022-08-22 16:20:44 +0530

    Msg Nice

--- Greatestofthree.py
+++ Greatestofthree.py
@@ -7,4 +7,4 @@ b = eval(input(print(f"Enter the Second Number :", end="\n")))
 c = eval(input(print(f"Enter the Third Number :", end="\n")))
 
 print(f"Greatest of 3 number is : {a if (a >= b and a >= c) else b if (b >= a and b >= c) else c}", end="\n")
-print("Have A Good Day!")
+print("Have A Nice Day!")
